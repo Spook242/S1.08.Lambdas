f79c09097ed9eba1986d1e3954f1ff18f7fd4057
Spook242 <spook242@gmail.com>
2025-10-16 13:18:45 +0200

    Shared in branch main.

--- src/nivel1Ejercicio3/Main.java
+++ src/nivel1Ejercicio3/Main.java
@@ -1,4 +1,19 @@
 package nivel1Ejercicio3;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+
+    static void main(String[] args) {
+
+        List<String> months = Arrays.asList(
+                "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+        );
+
+        System.out.println("Months of the year:");
+
+        months.forEach(month -> System.out.println(month));
+    }
 }
