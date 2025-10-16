13d9b02523471b276e5f87882a706bf5fdd03740
Spook242 <spook242@gmail.com>
2025-10-16 10:34:32 +0200

    Shared in branch main.

--- src/nivel1Ejercicio1/Main.java
+++ src/nivel1Ejercicio1/Main.java
@@ -1,4 +1,24 @@
 package nivel1Ejercicio1;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
+
+    static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Lewandowski", "Torres", "Yamal", "Pedri", "Raphinha", "Olmo", "Koundé");
+
+        List<String> ambO = filterO(names);
+
+        System.out.println("Names that contain the letter 'o':");
+        ambO.forEach(System.out::println);
+    }
+
+    public static List<String> filterO(List<String> namesList) {
+        return namesList.stream()
+                .filter(n -> n.contains("o"))
+                .collect(Collectors.toList());
+    }
 }
